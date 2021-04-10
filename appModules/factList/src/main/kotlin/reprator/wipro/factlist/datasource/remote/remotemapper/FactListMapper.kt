380cda185aabd617a718df110b69c147137d6255
Vikram Singh <mr.vikram004@gmail.com>
2021-04-10 17:30:46 +0530

    mapper added to convert the data from raw pojo to ui pojo(FactModal) with null checks

---
+++ appModules/factList/src/main/kotlin/reprator/wipro/factlist/datasource/remote/remotemapper/FactListMapper.kt
@@ -0,0 +1,15 @@
+package reprator.wipro.factlist.datasource.remote.remotemapper
+
+import reprator.wipro.base.util.Mapper
+import reprator.wipro.factlist.datasource.remote.modal.FactListEntity
+import reprator.wipro.factlist.modals.FactModals
+import javax.inject.Inject
+
+class FactListMapper @Inject constructor() :
+    Mapper<FactListEntity, Pair<String, List<FactModals>>> {
+    override suspend fun map(from: FactListEntity): Pair<String, List<FactModals>> {
+        return Pair(from.title, from.rows.map {
+            FactModals(it.title, it.description, it.imageHref ?: "")
+        })
+    }
+}
