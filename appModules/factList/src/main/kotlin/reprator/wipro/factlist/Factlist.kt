865db596d88bca8cd27504a6245e62b262e1b25f
Vikram Singh <mr.vikram004@gmail.com>
2021-04-10 17:49:39 +0530

    xml converted for databinding

--- appModules/factList/src/main/kotlin/reprator/wipro/factlist/Factlist.kt
+++ appModules/factList/src/main/kotlin/reprator/wipro/factlist/Factlist.kt
@@ -1,7 +1,27 @@
 package reprator.wipro.factlist
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import reprator.wipro.factlist.databinding.FragmentFactlistBinding
 
-class Factlist: Fragment(R.layout.fragment_factlist){
+@AndroidEntryPoint
+class Factlist : Fragment(R.layout.fragment_factlist) {
 
+    private var _binding: FragmentFactlistBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        _binding = FragmentFactlistBinding.bind(view).also {
+            it.lifecycleOwner = viewLifecycleOwner
+        }
+    }
 }
