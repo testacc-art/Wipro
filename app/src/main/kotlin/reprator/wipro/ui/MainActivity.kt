12e76161255b1e63a21da28848fe5222c4e77132
Vikram Singh <mr.vikram004@gmail.com>
2021-04-10 16:36:56 +0530

    MainActivity.kt is now linked with FactList fragment with the help of navigation module

---
+++ app/src/main/kotlin/reprator/wipro/ui/MainActivity.kt
@@ -0,0 +1,19 @@
+package reprator.wipro.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+import reprator.wipro.databinding.ActivityMainBinding
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
