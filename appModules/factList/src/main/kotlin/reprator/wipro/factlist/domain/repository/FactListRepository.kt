8ac6f34ef9a82531d641cf42d9b85ddb4d0e0a58
Vikram Singh <mr.vikram004@gmail.com>
2021-04-10 17:04:42 +0530

    FactListRepository.kt added for interaction with usecase

---
+++ appModules/factList/src/main/kotlin/reprator/wipro/factlist/domain/repository/FactListRepository.kt
@@ -0,0 +1,10 @@
+package reprator.wipro.factlist.domain.repository
+
+import kotlinx.coroutines.flow.Flow
+import reprator.wipro.base.useCases.AppResult
+import reprator.wipro.factlist.modals.FactModals
+
+interface FactListRepository {
+    suspend fun getFactListRepository():
+            Flow<AppResult<List<FactModals>>>
+}
