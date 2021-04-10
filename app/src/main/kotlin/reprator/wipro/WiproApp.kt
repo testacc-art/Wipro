76bdc798d17f7a2436706292a13f6bda5302b0f9
Vikram Singh <mr.vikram004@gmail.com>
2021-04-10 16:55:01 +0530

    Timber default setup

--- app/src/main/kotlin/reprator/wipro/WiproApp.kt
+++ app/src/main/kotlin/reprator/wipro/WiproApp.kt
@@ -2,6 +2,12 @@ package reprator.wipro
 
 import androidx.multidex.MultiDexApplication
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class WiproApp : MultiDexApplication()
+class WiproApp  : MultiDexApplication(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
