95a03d8b264920c799522ac747e604b2ba2b9c18
Vikram Singh <mr.vikram004@gmail.com>
2021-04-12 09:20:13 +0530

    debug flavour added for ui test of fragment to act as container

---
+++ appModules/factList/src/debug/kotlin/reprator/wipro/factlist/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package reprator.wipro.factlist
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
