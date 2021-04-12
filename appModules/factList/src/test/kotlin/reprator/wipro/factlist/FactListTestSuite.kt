8be502e72f4efa20c57d78007b1cb380b8a18a6d
Vikram Singh <mr.vikram004@gmail.com>
2021-04-12 07:45:28 +0530

    test suite added to run all unit test at once

---
+++ appModules/factList/src/test/kotlin/reprator/wipro/factlist/FactListTestSuite.kt
@@ -0,0 +1,20 @@
+package reprator.wipro.factlist
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import reprator.wipro.factlist.data.repositoryImpl.FactListDataRepositoryImplTest
+import reprator.wipro.factlist.datasource.remote.FactListApiServiceTest
+import reprator.wipro.factlist.datasource.remote.FactListRemoteDataSourceImplTest
+import reprator.wipro.factlist.datasource.remote.remotemapper.FactListMapperTest
+import reprator.wipro.factlist.domain.usecase.FactListUseCaseTest
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    FactListDataRepositoryImplTest::class,
+    FactListMapperTest::class,
+    FactListApiServiceTest::class,
+    FactListRemoteDataSourceImplTest::class,
+    FactListUseCaseTest::class,
+    FactListViewModalTest::class,
+)
+class FactListTestSuite
