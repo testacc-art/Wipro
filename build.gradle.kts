2cc6395aec4121b3608309d7f3876ec51c3ea78e
Vikram Singh <mr.vikram004@gmail.com>
2021-06-23 17:44:24 +0530

    dokka not hiding inherited member and showing comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,4 +27,101 @@ allprojects {
 
 subprojects {
     plugins.apply("org.jetbrains.dokka")
+
+    print("dokkaHtmlVikram 0")
+
+    tasks.named<org.jetbrains.dokka.gradle.DokkaTask>("dokkaHtml").configure {
+        print("dokkaHtmlVikram 1")
+        dokkaSourceSets {
+            named("main") {
+                print("dokkaHtmlVikram 2")
+                noAndroidSdkLink.set(false)
+                suppressInheritedMembers.set(true)
+            }
+        }
+    }
+
+    tasks.named<org.jetbrains.dokka.gradle.DokkaTask>("dokkaHtml") {
+        print("dokkaHtmlVikram 11")
+        dokkaSourceSets {
+            named("main") {
+                print("dokkaHtmlVikram 12")
+                noAndroidSdkLink.set(true)
+                suppressInheritedMembers.set(true)
+            }
+        }
+    }
+
+
+    tasks.dokkaHtml.configure {
+        print("dokkaHtmlVikram 51")
+
+        dokkaSourceSets {
+            named("main") { /* configure main source set */
+                print("dokkaHtmlVikram 52")
+            }
+            configureEach {  /* configure all source sets */
+                print("dokkaHtmlVikram 53")
+            }
+            register("custom") { /* register custom source set */
+                print("dokkaHtmlVikram 54")
+            }
+        }
+    }
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
+    suppressInheritedMembers.set(true)
+    print("dokkaHtmlVikram 21")
+    dokkaSourceSets {
+        named("main") {
+            print("dokkaHtmlVikram 22")
+            noAndroidSdkLink.set(true)
+            suppressInheritedMembers.set(true)
+        }
+    }
+}
+
+tasks.dokkaHtml.configure {
+    print("dokkaHtmlVikram 31")
+
+    dokkaSourceSets {
+        named("main") { /* configure main source set */
+            print("dokkaHtmlVikram 32")
+        }
+        configureEach {  /* configure all source sets */
+            print("dokkaHtmlVikram 33")
+        }
+        register("custom") { /* register custom source set */
+            print("dokkaHtmlVikram 34")
+        }
+    }
+}
+
+tasks.dokkaHtmlMultiModule.configure {
+    print("dokkaHtmlVikram 41")
+    suppressInheritedMembers.set(true)
+    pluginsMapConfiguration.set(
+        mapOf("org.jetbrains.dokka.base.DokkaBase" to """{ "separateInheritedMembers": true}""")
+    )
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTask>().configureEach {
+    print("dokkaHtmlVikram 71")
+    dokkaSourceSets {
+        print("dokkaHtmlVikram 72")
+        named("main") {
+            print("dokkaHtmlVikram 73")
+            sourceLink {
+                print("dokkaHtmlVikram 74")
+            }
+        }
+    }
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaTaskPartial>() {
+    print("dokkaHtmlVikram 81")
+    pluginsMapConfiguration.set(
+        mapOf("org.jetbrains.dokka.base.DokkaBase" to """{ "separateInheritedMembers": true}""")
+    )
 }
