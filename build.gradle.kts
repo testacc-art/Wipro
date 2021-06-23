77f06875d1f86bb8acb206c78f29447719c1b176
Vikram Singh <mr.vikram004@gmail.com>
2021-06-23 13:19:26 +0530

    spotless configured but not running

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,20 +26,25 @@ allprojects {
 }
 
 subprojects {
+
     apply(plugin = "com.diffplug.spotless")
+
     configure<com.diffplug.gradle.spotless.SpotlessExtension> {
         kotlin {
             target ("**/*.kt")
             targetExclude("$buildDir/**/*.kt")
             targetExclude("bin/**/*.kt")
 
-            ktlint("0.40.0")
+            ktlint("0.40.0")   // has its own section below
+            ktfmt()    // has its own section below
+            prettier() // has its own section below
+
             licenseHeaderFile("${project.rootProject.projectDir}/spotless/copyright.kt")
         }
 
-        format ("xml") {
-            target ("**/*.xml")
-            prettier(mapOf("prettier" to "2.0.5", "@prettier/plugin-xml" to "0.13.0")).config(mapOf("parser" to "xml", "tabWidth" to 4)) // this is erroneous
+        kotlinGradle {
+            target("*.gradle.kts") // default target for kotlinGradle
+            ktlint() // or ktfmt() or prettier()
         }
     }
 }
